dfaef7d5bfcb7323a30090eeea3d34a64f660ac2
Andrie <apdevries@gmail.com>
2019-09-16 17:22:42 +0100

    Install packages using binaries from RSPM

--- _prereqs.R
+++ _prereqs.R
@@ -1,9 +1,19 @@
+# Use RStudio Package Manager binary package installation
+# https://docs.rstudio.com/rspm/1.0.12/admin/binaries.html#binary-user-agents
+
+options(repos = RSPM)
+RSPM <- c(CRAN = "http://cluster.rstudiopm.com/cran/__linux__/bionic/latest")
+
+# Set the default HTTP user agent
+options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), 
+                                paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
+
 pkgs <- c(
   "tidyverse",
   "babynames",
   "splines",
   
-  "repoman", 
+  "repoman",
   "shiny",
   "shinydashboard",
   "dbplyr",
